18a7cc6c603a070b6c004684d4664018bdadd939
Sergei Kamianetskii <i@ksergey.ru>
2023-12-17 17:30:37 +0300

    commit 005

--- Domain/Model/NumbersModel/NumbersPair.cs
+++ Domain/Model/NumbersModel/NumbersPair.cs
@@ -4,7 +4,5 @@ public class NumbersPair
 {
   public int Id { get; set; }
   public int Value { get; set; }
-  public int X { get; set; }
-  public int Y { get; set; }
   public string Description { get; set; }
 }
